f03833d2f3559fc2ec2fae1936ef405bc2f4107c
Seth Verrill <verrill.seth@gmail.com>
2024-10-21 16:25:39 -0600

    fix: fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "api/v1/tasks", to: "api/v1/tasks#index"
+  get "/api/v1/tasks", to: "api/v1/tasks#index"
   get "/api/v1/tasks/:id", to: "api/v1/tasks#show"
   post "/api/v1/tasks", to: "api/v1/tasks#create"
   patch "/api/v1/tasks/:id", to: "api/v1/tasks#update"
